c6be0cb3a3d9f22bd3e76a446bc43b33f62fbbad
iegupov <igor.iegupov@gmail.com>
2017-12-08 15:08:51 +0100

    Adjust license headers

--- src/Installer/PackageInstallerTrigger.php
+++ src/Installer/PackageInstallerTrigger.php
@@ -1,23 +1,7 @@
 <?php
 /**
- * This file is part of OXID eShop Composer plugin.
- *
- * OXID eShop Composer plugin is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * OXID eShop Composer plugin is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with OXID eShop Composer plugin.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @link      http://www.oxid-esales.com
- * @copyright (C) OXID eSales AG 2003-2016
- * @version   OXID eShop Composer plugin
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
  */
 
 namespace OxidEsales\ComposerPlugin\Installer;
