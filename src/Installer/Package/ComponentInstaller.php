db14dc4ff6ccf95d85ef1e95fa088a8b065bb8cb
iegupov <igor.iegupov@gmail.com>
2019-01-25 14:29:24 +0100

    OXDEV-1285 Use bootstrap container in ComponentInstaller

--- src/Installer/Package/ComponentInstaller.php
+++ src/Installer/Package/ComponentInstaller.php
@@ -8,39 +8,11 @@ namespace OxidEsales\ComposerPlugin\Installer\Package;
 
 use Composer\IO\IOInterface;
 use Composer\Package\PackageInterface;
-use OxidEsales\EshopCommunity\Internal\Application\Dao\ProjectYamlDao;
-use OxidEsales\EshopCommunity\Internal\Application\Service\ProjectYamlImportService;
+use OxidEsales\EshopCommunity\Internal\Application\BootstrapContainer\BootstrapContainerFactory;
 use OxidEsales\EshopCommunity\Internal\Application\Service\ProjectYamlImportServiceInterface;
-use OxidEsales\EshopCommunity\Internal\Application\Utility\BasicContext;
 
 class ComponentInstaller extends AbstractPackageInstaller
 {
-    /**
-     * @var ProjectYamlImportService
-     */
-    private $projectYamlImportService;
-
-    /**
-     * @param IOInterface $io
-     * @param string $rootDirectory
-     * @param PackageInterface $package
-     * @param ProjectYamlImportServiceInterface|null $projectYamlImportService
-     */
-    public function __construct(
-        IOInterface $io,
-        $rootDirectory,
-        PackageInterface $package
-    ) {
-        parent::__construct($io, $rootDirectory, $package);
-
-        $context = new BasicContext();
-        $projectYamlDao = new ProjectYamlDao($context);
-        $this->projectYamlImportService = new ProjectYamlImportService($projectYamlDao);
-        if (file_exists($context->getContainerCacheFilePath())) {
-            unlink($context->getContainerCacheFilePath());
-        }
-    }
-
     public function install($packagePath)
     {
         $this->writeInstallingMessage("component");
@@ -58,7 +30,8 @@ class ComponentInstaller extends AbstractPackageInstaller
      */
     protected function importServiceFile($packagePath)
     {
-        $this->projectYamlImportService->removeNonExistingImports();
-        $this->projectYamlImportService->addImport($packagePath);
+        $projectYamlImportService = BootstrapContainerFactory::getBootstrapContainer()->get(ProjectYamlImportServiceInterface::class);
+        $projectYamlImportService->removeNonExistingImports();
+        $projectYamlImportService->addImport($packagePath);
     }
 }
