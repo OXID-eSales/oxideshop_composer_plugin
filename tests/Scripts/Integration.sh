1a677a09200c85f9bb9b7f83f34221ec8bf184fb
Jörn Ott <joern.ott@ott-consult.de>
2024-04-26 14:28:58 +0200

    Update Review results

--- tests/Scripts/Integration.sh
+++ tests/Scripts/Integration.sh
@@ -2,7 +2,6 @@
 set -e
 vendor/bin/phpunit \
     -c phpunit.xml \
-    --bootstrap tests/bootstrap.php \
     --coverage-clover=tests/Reports/coverage_phpunit_integration.xml \
     tests/Integration 2>&1 \
 | tee tests/Output/integration_tests.txt
